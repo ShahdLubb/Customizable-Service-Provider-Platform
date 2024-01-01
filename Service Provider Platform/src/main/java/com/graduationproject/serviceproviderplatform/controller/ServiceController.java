af325c0f20b791aadd4bca1ba077f77a842484f8
yaqout salameh <yaqoutsalameh@gmail.com>
2024-01-02 00:43:03 +0200

    Introduce getAllServices API and refine getCategoryServices

--- Service Provider Platform/src/main/java/com/graduationproject/serviceproviderplatform/controller/ServiceController.java
+++ Service Provider Platform/src/main/java/com/graduationproject/serviceproviderplatform/controller/ServiceController.java
@@ -1,6 +1,5 @@
 package com.graduationproject.serviceproviderplatform.controller;
 
-import com.graduationproject.serviceproviderplatform.model.Category;
 import com.graduationproject.serviceproviderplatform.model.Service;
 import com.graduationproject.serviceproviderplatform.repository.ServiceRepository;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +17,9 @@ public class ServiceController {
         this.serviceRepository = serviceRepository;
     }
 
-    // Here will be the filter API
-    // Other service APIs are on the CategoryController
+    @GetMapping
+    public ResponseEntity<List<Service>> getAllServices() {
+        List<Service> services = serviceRepository.findAll();
+        return ResponseEntity.ok(services);
+    }
 }
